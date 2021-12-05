0afe5f8b20ec775a14b18ece9ec5870ca4e96374
bartshin <bartshin@icloud.com>
2021-12-05 09:44:14 +0900

    2021.12.05 morning

--- PixelScheduler/LocationPickerView.swift
+++ PixelScheduler/LocationPickerView.swift
@@ -15,25 +15,34 @@ struct LocationPickerView: View {
 	@State private var searchString = ""
 	@State private var showingRegion = MKCoordinateRegion(center: LocationManager.seoulLocation, span: LocationManager.cityBounds)
 	@State private var isShowingResultView = false
+	@State private var searchResults = [Place]()
 	
-    var body: some View {
+	
+	private struct Place: Identifiable {
+		let name: String
+		let coordinate: CLLocationCoordinate2D
+		var id: String {
+			"\(coordinate.latitude), \(coordinate.longitude)"
+		}
+	}
+	var body: some View {
 		GeometryReader{ geometry in
 			VStack {
 				TextField(settingController.language == .korean ? "장소 검색어를 입력하세요": "Keyword for searching location", text: $searchString,
-						  onCommit: {
-					DispatchQueue.main.async {
-						withAnimation {
-							isShowingResultView.toggle()
-						}
+									onCommit: {
+					if !searchString.isEmpty {
+						searchLocation(keyword: searchString)
 					}
 				})
 					.textFieldStyle(.roundedBorder)
 				ZStack {
-					Map(coordinateRegion: $showingRegion)
+					Map(coordinateRegion: $showingRegion, showsUserLocation: true, annotationItems: searchResults) {
+						MapMarker(coordinate: $0.coordinate, tint: .blue)
+					}
 					if isShowingResultView {
 						searchResultView
 							.frame(width: geometry.size.width * 0.5,
-								   height: 300)
+										 height: 300)
 							.offset(x: geometry.size.width * 0.25)
 							.transition(.move(edge: .trailing))
 					}
@@ -53,7 +62,27 @@ struct LocationPickerView: View {
 				}
 			}
 		}
-    }
+	}
+	
+	private func searchLocation(keyword: String) {
+		
+		let request = MKLocalSearch.Request()
+		request.naturalLanguageQuery = keyword
+		
+		MKLocalSearch(request: request).start { response, error in
+			guard error == nil,
+						let response = response else{
+							print("Error to search location \(error?.localizedDescription ?? "")")
+							return
+						}
+			DispatchQueue.main.async {
+				searchResults = response.mapItems.compactMap {
+					Place(name: $0.name ?? "", coordinate: $0.placemark.coordinate)
+				}
+			}
+		}
+		
+	}
 	
 	private var searchResultView: some View {
 		List {
@@ -64,9 +93,9 @@ struct LocationPickerView: View {
 }
 
 struct LocationPickerView_Previews: PreviewProvider {
-    static var previews: some View {
+	static var previews: some View {
 		LocationPickerView()
 			.previewLayout(.fixed(width: 350, height: 400))
-				.environmentObject(SettingController())
-    }
+			.environmentObject(SettingController())
+	}
 }
