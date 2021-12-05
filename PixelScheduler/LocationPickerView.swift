52f0e84ffb52f40bcb3a7c1e92fa2e2305262c31
bartshin <bartshin@icloud.com>
2021-12-05 16:41:53 +0900

    2021.12.05 supper

--- PixelScheduler/LocationPickerView.swift
+++ PixelScheduler/LocationPickerView.swift
@@ -14,9 +14,13 @@ struct LocationPickerView: View {
 	@StateObject private var location = LocationManager()
 	@State private var searchString = ""
 	@State private var showingRegion = MKCoordinateRegion(center: LocationManager.seoulLocation, span: LocationManager.cityBounds)
-	@State private var isShowingResultView = false
+	@State private var isShowingResultView = false {
+		didSet {
+			moveSearchResultView()
+		}
+	}
 	@State private var searchResults = [Place]()
-	
+	@State private var searchResultViewOffset: CGFloat = 0.75
 	
 	private struct Place: Identifiable {
 		let name: String
@@ -29,23 +33,21 @@ struct LocationPickerView: View {
 		GeometryReader{ geometry in
 			VStack {
 				TextField(settingController.language == .korean ? "장소 검색어를 입력하세요": "Keyword for searching location", text: $searchString,
-									onCommit: {
+						  onCommit: {
 					if !searchString.isEmpty {
 						searchLocation(keyword: searchString)
 					}
+					isShowingResultView = true
 				})
 					.textFieldStyle(.roundedBorder)
 				ZStack {
 					Map(coordinateRegion: $showingRegion, showsUserLocation: true, annotationItems: searchResults) {
 						MapMarker(coordinate: $0.coordinate, tint: .blue)
 					}
-					if isShowingResultView {
-						searchResultView
-							.frame(width: geometry.size.width * 0.5,
-										 height: 300)
-							.offset(x: geometry.size.width * 0.25)
-							.transition(.move(edge: .trailing))
-					}
+					searchResultView
+						.frame(width: geometry.size.width * 0.5,
+							   height: 300)
+						.offset(x: geometry.size.width * searchResultViewOffset)
 				}
 				.frame(width: geometry.size.width, height: 300)
 			}
@@ -64,31 +66,52 @@ struct LocationPickerView: View {
 		}
 	}
 	
+	private func moveSearchResultView() {
+		DispatchQueue.main.async {
+			withAnimation {
+				searchResultViewOffset = isShowingResultView ? 0.25: 0.75
+			}
+		}
+	}
+	
 	private func searchLocation(keyword: String) {
 		
 		let request = MKLocalSearch.Request()
 		request.naturalLanguageQuery = keyword
-		
 		MKLocalSearch(request: request).start { response, error in
 			guard error == nil,
-						let response = response else{
-							print("Error to search location \(error?.localizedDescription ?? "")")
-							return
-						}
+				  let response = response else{
+					  print("Error to search location \(error?.localizedDescription ?? "")")
+					  return
+				  }
 			DispatchQueue.main.async {
 				searchResults = response.mapItems.compactMap {
 					Place(name: $0.name ?? "", coordinate: $0.placemark.coordinate)
 				}
 			}
 		}
-		
 	}
 	
 	private var searchResultView: some View {
 		List {
-			Text("Search result")
+			ForEach(searchResults) { place in
+				Text(place.name)
+					.onTapGesture {
+						withAnimation {
+							showingRegion = MKCoordinateRegion(center: place.coordinate, span: LocationManager.streetBounds)
+						}
+					}
+			}
 		}
 		.clipShape(RoundedRectangle(cornerRadius: 30))
+		.gesture(
+			DragGesture()
+				.onEnded { gestureValue in
+					if gestureValue.translation.width > 50 {
+						isShowingResultView = false
+					}
+				}
+		)
 	}
 }
 
